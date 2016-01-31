265a7223cd8acdf3ae675cd989add506744fbcdc
Mircea Catana <mircea.catalin.catana@gmail.com>
2016-01-31 10:38:41 +0000

    Game HUD

---
+++ goldsmiths_gamejam/Assets/Scripts/GameHUD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameHUD : MonoBehaviour {
+
+    public Text dayLabel;
+    public Text illLabel;
+    public Text populationLabel;
+
+    private GameManager gm;
+
+	void Start () {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	void Update () {
+        dayLabel.text = "Day: " + gm.currentDay;
+        illLabel.text = "Wounded: " + gm.illPopulation;
+        populationLabel.text = "Tribe Warriors: " + gm.currentPopulation;
+	}
+}
